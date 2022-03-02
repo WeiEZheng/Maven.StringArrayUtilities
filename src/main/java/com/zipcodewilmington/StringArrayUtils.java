fd587cb183ec7afacb01fb0cc5e3abe45506c78a
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-02 17:21:43 -0500

    finished remove

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -97,7 +97,13 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+        ArrayList <String> arrayCopy = new ArrayList<String>();
+        for (int i=0;i< array.length;i++){
+            if (!array[i].equals(valueToRemove)){
+                arrayCopy.add(array[i]);
+            }
+        }
+        return arrayCopy.toArray(new String[arrayCopy.size()]);
     }
 
     /**
